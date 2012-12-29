191b2985c743a2b22bd75456071bcaf159aff87c
appsaru <arunbeee@in.com>
2012-12-29 08:12:38 +0530

    Making Dynamic

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,7 +5,11 @@ describe "StaticPages" do
 
   it "should have the content 'Sample App'" do
     visit '/static_pages/home'
-    page.should have_content('Sample App')
+    page.should have_selector('h1' , :text => 'Sample App')
+  end
+ it "should have the content 'Sample App'" do
+    visit '/static_pages/home'
+    page.should have_selector('title' , :text => 'Ruby on Rails Tutorial Sample App|Home')
   end
 end
 
@@ -13,7 +17,11 @@ end
 
 	it "should have content 'Help'" do 
 	visit '/static_pages/help'
-	page.should have_content('Help')
+	page.should have_selector('h1', :text => 'Help')
+     end
+it "should have content 'Help'" do 
+	visit '/static_pages/help'
+	page.should have_selector('title', :text => 'Ruby on Rails Tutorial Sample App | Help')
      end
  end
 
@@ -21,7 +29,11 @@ end
 
 	it "should have content 'Help'" do 
 	visit '/static_pages/about'
-	page.should have_content('About')
+	page.should have_selector('h1', :text => 'About')
+     end
+it "should have content 'About'" do 
+	visit '/static_pages/about'
+	page.should have_selector('title', :text => 'Ruby on Rails Tutorial Sample App | About')
      end
  end
 end
